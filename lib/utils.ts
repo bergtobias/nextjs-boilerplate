36ddf5cd85b6af5f30e27194dd43f24b5abf1e11
nilsdacke1 <nilsdacke1@gmail.com>
2024-04-14 00:31:01 +0200

    first commit

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import prisma from "./db";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
